7a2451913c9574fc54a1d2f7e96507f6a013c874
Lisa Van Huijkelom <lisa.vanhuijkelom@infinicept.com>
2023-06-22 18:52:35 -0600

    Adding angular components for project

--- angular-cooking-class/src/app/app-routing.module.ts
+++ angular-cooking-class/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', component: HomeComponent },
+{ path: 'login', component: LoginComponent },
+{ path: 'favorites', component: FavoritesComponent },
+{ path: 'recipe-detail', component: RecipeDetailComponent },
+{ path: 'recipe-detail/:id', component: RecipeDetailComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-cooking-class/src/app/app.module.ts
+++ angular-cooking-class/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavigationComponent,
+    LoginComponent,
+    RecipeDetailComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
